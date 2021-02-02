05084d5403f2b3b9d2510e67ea87fe987fb60bc5
zht2649825643 <2649825643@qq.com>
2021-02-02 09:54:07 +0800

    2021-02-02

---
+++ code3_fork/1-fork.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "sys/types.h"
+
+int g_var = 99;
+
+int main(void)
+{
+	pid_t pid = fork();
+
+	if(pid < 0)
+	{
+		printf("fork error");
+		return -1;
+	}
+	if(pid > 0)
+	{
+		g_var = 0;
+		printf("father pid == [%d] \t g_var == [%d]\n",getpid(),g_var);
+	}
+	if(pid == 0)
+	{
+		printf("child pid == [%d] \t g_var == [%d]\n",getpid(),g_var);
+	}
+
+	return 1;
+}
+
