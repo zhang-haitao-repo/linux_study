a32492765819186ce2289b1a3404c06173411ab4
zht2649825643 <2649825643@qq.com>
2021-03-06 05:07:29 -0800

    2021-new_note

--- github.sh
+++ github.sh
@@ -1,7 +1,7 @@
 git config  --global user.name zht2649825643
 git config  --global user.email 2649825643@qq.com
 git remote rm main
-git remote add main https://github.com/zht2649825643/linux_study.git  
+git remote add origin https://github.com/zht2649825643/linux_study.git  
 git add .
 git commit -m "2021-new_note"
 git push -u origin main
